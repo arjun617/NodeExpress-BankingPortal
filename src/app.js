900c8fdcd2d2490f5b240887767dc84f0be97687
arjun617 <arjundhall1995@gmail.com>
2018-11-14 15:44:51 +0530

    Completed the module

--- src/app.js
+++ src/app.js
@@ -9,6 +9,9 @@ app.set('view engine', 'ejs')
 
 app.use(express.static(path.join(__dirname, 'public')))
 
+const accountData = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'), 'utf8')
+const accounts = JSON.parse(accountData)
+
 app.get('/', (req, res) => res.render('index', {
   title: 'Index'
 }))
