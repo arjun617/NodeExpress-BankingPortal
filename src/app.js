1d54bcaf77b9142a496d2ba757ef96f157c8a4cd
arjun617 <arjundhall1995@gmail.com>
2018-11-14 15:53:57 +0530

    Completed the module

--- src/app.js
+++ src/app.js
@@ -24,6 +24,14 @@ app.get('/savings', (req, res) => res.render('account', {
   account: accounts.savings
 }))
 
+app.get('/checking', (req, res) => res.render('account', {
+  account: accounts.checking
+}))
+
+app.get('/credit', (req, res) => res.render('account', {
+  account: accounts.credit
+}))
+
 app.listen(3000, () => {
   console.log('PS Project Running on port 3000!')
 })
