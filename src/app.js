075b9de0220476500f81ec65899ed8d3a1acd794
arjun617 <arjundhall1995@gmail.com>
2018-11-14 16:16:43 +0530

    Completed the module

--- src/app.js
+++ src/app.js
@@ -41,6 +41,15 @@ app.get('/profile', (req, res) => res.render('profile', {
 }))
 
 app.get('/transfer', (req, res) => res.render('transfer'))
+
+app.post('/transfer', (req, res) => {
+  accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount
+  accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount, 10)
+  const accountsJSON = JSON.stringify(accounts, null, 4)
+  fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf8')
+})
+
+
 app.listen(3000, () => {
   console.log('PS Project Running on port 3000!')
 })
