ba947479b508a816952ae41149f4c5ca2f12106c
arjun617 <arjundhall1995@gmail.com>
2018-11-14 16:21:19 +0530

    Completed the module

--- src/app.js
+++ src/app.js
@@ -47,6 +47,26 @@ app.post('/transfer', (req, res) => {
   accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount, 10)
   const accountsJSON = JSON.stringify(accounts, null, 4)
   fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf8')
+  res.render('transfer', {
+    message: "Transfer Completed"
+  })
+})
+
+app.get('/payment', (req, res) => {
+  res.render('payment', {
+    account: accounts.credit
+  })
+})
+
+app.post('/payment', (req, res) => {
+  accounts.credit.balance = accounts.credit.balance - req.body.amount
+  accounts.credit.available = parseInt(accounts.credit.available) + parseInt(req.body.amount)
+  const accountsJSON = JSON.stringify(accounts, null, 4)
+  fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf8')
+  res.render('payment', {
+    message: "Payment Successful",
+    account: accounts.credit
+  })
 })
 
 
